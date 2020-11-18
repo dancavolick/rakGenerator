d75ff9c8239beee77372d105b14f5a52cd09cf8f
Daniel Cavolick <dancavolick@yahoo.co.uk>
2020-11-18 14:11:49 +0000

    Tidy up

--- rak.js
+++ rak.js
@@ -5,8 +5,6 @@ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 console.log(date);
 
 
-function randomDate(end) {
-    return new Date(today.getTime() + Math.random() * (end.getTime() - today.getTime()));
-  }
-  
-  console.log(randomDate();
+
+
+
